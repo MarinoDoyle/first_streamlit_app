30d212d0ccb640ed0215929cc66b7927b76061b0
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-11-27 10:52:05 +1300

    Update streamlit_app.py
    Building out breakfast favourites

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Favourites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text(' 🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avacado Toast')

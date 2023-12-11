0457b208e85530fe77e1c513de85acde05bc4aa7
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-12-12 09:49:20 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,56 @@
-import streamlit
-import pandas 
-import requests
-import snowflake.connector
-from urllib.error import URLError
+import streamlit as st
+import os
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import LLMChain
+from langchain.llms import OpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain.document_loaders.csv_loader import CSVLoader
 
-# Using an OPENAI key
-os.environ['OPENAI_API_KEY'] = "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"
+st.sidebar.title("Enter API Key")
+api_key = st.sidebar.text_input("API Key:")
 
-llm = ChatOpenAI()
+if api_key:
+    os.environ['OPENAI_API_KEY'] = api_key
 
-streamlit.title("Chat-Based Language Model")
+st.title("Chat-Based Language Model")
 
-question = streamlit.text_input("Enter your question here:")
+question = st.text_input("Enter your question here:")
 
-if streamlit.button("Ask"):
-    loader = CSVLoader(file_path=r"survey.csv")
-    data = loader.load()
+if st.button("Ask"):
+    if 'OPENAI_API_KEY' not in os.environ:
+        st.error("Please enter your API Key in the sidebar.")
+    else:
+        loader = CSVLoader(file_path=r"C:\Users\Marino Doyle\Desktop\Te Tihi Project\Te-Tihi-Project\dummyData\survey_data_extra.csv")
+        data = loader.load()
 
-    text_splitter_csv = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-    all_splits_csv = text_splitter_csv.split_documents(data)
+        text_splitter_csv = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+        all_splits_csv = text_splitter_csv.split_documents(data)
 
-    vector_store = FAISS.from_documents(all_splits_csv, HuggingFace())
+        vector_store = FAISS.from_documents(all_splits_csv, OpenAIEmbeddings())
 
-    retriever = vector_store.as_retriever()
+        retriever = vector_store.as_retriever()
 
-    template = """Answer the question based only on the following context:
-    {context}
-    
-    Question: {question}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
+        template = """Answer the question based only on the following context:
+        {context}
+        
+        Question: {question}
+        """
+        prompt = ChatPromptTemplate.from_template(template)
 
-    chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-    )
+        llm = ChatOpenAI()
 
-    response = chain.invoke(question)
-    streamlit.text_area("Response:", value=response)
+        chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
 
-
+        response = chain.invoke(question)
+        st.text_area("Response:", value=response)

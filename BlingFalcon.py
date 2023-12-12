e42410715b912290fec68b75f792acc7bd5c9738
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-12-13 09:52:50 +1300

    Update BlingFalcon.py

--- BlingFalcon.py
+++ BlingFalcon.py
@@ -1,22 +1,25 @@
 import streamlit as st 
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import transformers
+from transformers import GPT2Tokenizer, GPT2Model
 from langchain.llms import HuggingFacePipeline
 
-tokenizer = AutoTokenizer.from_pretrained("llmware/bling-falcon-1b-0.1")
-model = AutoModelForCausalLM.from_pretrained("llmware/bling-falcon-1b-0.1")
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2Model.from_pretrained('gpt2')
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+st.write(output = model(**encoded_input))
 
 
-generate_text = transformers.pipeline(
-    model=model,
-    tokenizer=tokenizer,
-    return_full_text=True,  # langchain expects the full text
-    task='text-generation',
-    # temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
-    max_new_tokens=512,  # max number of tokens to generate in the output
-    repetition_penalty=1.1  # without this output begins repeating
-)
+# generate_text = transformers.pipeline(
+#     model=model,
+#     tokenizer=tokenizer,
+#     return_full_text=True,  # langchain expects the full text
+#     task='text-generation',
+#     # temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+#     max_new_tokens=512,  # max number of tokens to generate in the output
+#     repetition_penalty=1.1  # without this output begins repeating
+# )
 
-res = generate_text("Explain me the difference a Server and a Client")
-st.write((res[0]["generated_text"]))
+# res = generate_text("Explain me the difference a Server and a Client")
+# st.write((res[0]["generated_text"]))
 

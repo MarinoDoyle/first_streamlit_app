116b1d9405a8ca7292942803b0dfba808442a341
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-12-12 13:08:12 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,18 @@ if api_key:
         verbose=True,
         agent_type=AgentType.OPENAI_FUNCTIONS,
     )
-    
+
+    if uploaded_file :
+   #use tempfile because CSVLoader only accepts a file_path
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
+
+    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
+                'delimiter': ','})
+    data = loader.load()
+
+    st.write(data)
 # Creating text embeddings and vector database
 #loader = CSVLoader(file_path="work_dummy_data.csv")
 #data = loader.load()

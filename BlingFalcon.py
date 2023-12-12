1a2cee9cbe95888e081b692ab690778bbe581fd1
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-12-13 10:40:34 +1300

    Update BlingFalcon.py

--- BlingFalcon.py
+++ BlingFalcon.py
@@ -4,6 +4,8 @@ from transformers import GPT2Tokenizer, GPT2Model, pipeline
 from langchain.llms import HuggingFacePipeline
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2Model.from_pretrained('gpt2')

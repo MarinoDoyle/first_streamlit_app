cd64640d3b4b6b6bc1c223d0820d05ccd4231320
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-12-13 10:03:23 +1300

    Update BlingFalcon.py

--- BlingFalcon.py
+++ BlingFalcon.py
@@ -8,7 +8,7 @@ model = GPT2Model.from_pretrained('gpt2')
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors='pt')
 output = model(**encoded_input)
-st.write(output)
+#st.write(output)
 # llm = 
 generator = pipeline('text-generation', model='gpt2')
 st.write(generator("What is Machine Learning", max_length=10, num_return_sequences=5))

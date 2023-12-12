0067c8d38488e3d0ce1fa5aec04b9115db6baad9
MarinoDoyle <150637268+MarinoDoyle@users.noreply.github.com>
2023-12-13 10:52:08 +1300

    Update BlingFalcon.py

--- BlingFalcon.py
+++ BlingFalcon.py
@@ -6,7 +6,9 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
-
+from langchain_core.runnables import RunnablePassthrough
+from langchain.schema import StrOutputParser
+from langchain import hub
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2Model.from_pretrained('gpt2')
@@ -32,3 +34,18 @@ vectorstore = FAISS.from_documents(all_splits_csv, embeddings)
 question = "What is the most frequent result for total number of rooms"
 embedding_vector = embeddings.embed_query(question)
 st.write(embedding_vector)
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+
+prompt = hub.pull("rlm/rag-prompt")
+
+# Creating a T
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+rag_chain.invoke("What are some key insights?")
